07f664d2d83241b47302e3f216e48b5b22dee5d6
AyeshaNasirWebDeveloper <ayeshanasir806@gmail.com>
2024-03-27 02:51:18 +0500

    first commit

---
+++ 2nd Assignment/two.js
@@ -0,0 +1,5 @@
+// Question 2:
+// Famous Quote 2: Repeat Exercise 1, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Albert Einstein ";
+var message = "".concat(famous_person, "once said, \u201CA person who never made a mistake never tried anything new.\u201D");
+console.log(message);
---
+++ 3rd Assignment/three.js
@@ -0,0 +1,4 @@
+// Question 3: Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Use "\t" (tab) and "\n" (new line) at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after stripping the white spaces.
+var personName = "\t\t\n\n Ayesha \t\t\n\n";
+console.log(personName);
+console.log(personName.trim());
